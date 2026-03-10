3e9226693da45d2e0e4872800a63bdd590b20adc
Gavendra <pratapgavendra01@gmail.com>
2026-03-10 11:11:28 +0530

    [Gavendra] Added.Solved UC-12

--- QuantityMeasurementApp/obj/Debug/net9.0/QuantityMeasurementApp.AssemblyInfo.cs
+++ QuantityMeasurementApp/obj/Debug/net9.0/QuantityMeasurementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd19aa001517c694daee13e4e1ba40c8446ec68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec622bac94067e3e41454446844e19b01202f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
