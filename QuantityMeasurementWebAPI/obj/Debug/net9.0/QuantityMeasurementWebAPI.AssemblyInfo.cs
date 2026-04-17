6a6c44cb480b3c13aac1234e7af7820eccaa8319
Gavendra <pratapgavendra01@gmail.com>
2026-04-17 16:35:46 +0530

    Recreate all missing .csproj files and essential source code

--- QuantityMeasurementWebAPI/obj/Debug/net9.0/QuantityMeasurementWebAPI.AssemblyInfo.cs
+++ QuantityMeasurementWebAPI/obj/Debug/net9.0/QuantityMeasurementWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurementWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9719bfb484c537c5a08edd5d2c32f3cfebb1f3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae6ac096d77854074b632b1a145c716ab7fb7045")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurementWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurementWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
