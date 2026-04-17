6368fbe4bcdc7a2a8847f4e89585220f9ff28d67
Gavendra <pratapgavendra01@gmail.com>
2026-04-17 17:31:28 +0530

    Fix missing Npgsql using statements in migration files

--- QuantityMeasurementRepositoryLayer/Migrations/QuantityMeasurementDbContextModelSnapshot.cs
+++ QuantityMeasurementRepositoryLayer/Migrations/QuantityMeasurementDbContextModelSnapshot.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using QuantityMeasurementRepositoryLayer.Context;
 
 #nullable disable
